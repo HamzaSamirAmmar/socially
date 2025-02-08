53c159bd81b9b7511ff9b5d9b9aa9531c53550e0
Hamza <h.ammar@staff.ososs.com>
2025-02-09 00:09:46 +0300

    Added missing isar dependncies

--- app/lib/locator.config.dart
+++ app/lib/locator.config.dart
@@ -10,13 +10,13 @@
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
-import 'package:isar/isar.dart' as _i5;
+import 'package:isar/isar.dart' as _i4;
 import 'package:socially/infrastructure/connection/connection_service.dart'
     as _i3;
 import 'package:socially/infrastructure/persistence/database_storage_service.dart'
-    as _i4;
-import 'package:socially/infrastructure/persistence/local_storage_service.dart'
     as _i6;
+import 'package:socially/infrastructure/persistence/local_storage_service.dart'
+    as _i5;
 
 extension GetItInjectableX on _i1.GetIt {
 // initializes the registration of main-scope dependencies inside of GetIt
@@ -36,12 +36,11 @@ extension GetItInjectableX on _i1.GetIt {
       () => connectionServiceRegister.create(),
       preResolve: true,
     );
-    gh.singleton<_i4.DatabaseSchemasPasser>(() => _i4.DatabaseSchemasPasser());
-    await gh.factoryAsync<_i5.Isar>(
-      () => databaseStorageRegister.create(gh<_i4.DatabaseSchemasPasser>()),
+    await gh.factoryAsync<_i4.Isar>(
+      () => databaseStorageRegister.create(),
       preResolve: true,
     );
-    await gh.factoryAsync<_i6.LocalStorageService>(
+    await gh.factoryAsync<_i5.LocalStorageService>(
       () => localStorageServiceRegister.create(),
       preResolve: true,
     );
@@ -51,6 +50,6 @@ extension GetItInjectableX on _i1.GetIt {
 
 class _$ConnectionServiceRegister extends _i3.ConnectionServiceRegister {}
 
-class _$DatabaseStorageRegister extends _i4.DatabaseStorageRegister {}
+class _$DatabaseStorageRegister extends _i6.DatabaseStorageRegister {}
 
-class _$LocalStorageServiceRegister extends _i6.LocalStorageServiceRegister {}
+class _$LocalStorageServiceRegister extends _i5.LocalStorageServiceRegister {}
